fdc315906ecba601c85adb6cfe5dfc5d4a7fc213
Wojciech Łapacz <wojciech.lapacz02@gmail.com>
2024-03-16 17:40:30 +0100

    training loop

---
+++ Task 1/main.py
@@ -0,0 +1,47 @@
+from torchvision.models import resnet18, ResNet18_Weights
+
+import torch
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision.transforms import Compose, PILToTensor
+from torchvision.transforms.functional import pil_to_tensor
+
+from contrastive_loss import ContrastiveLoss
+from custom_model import Encoder
+from taskdataset import TaskDataset
+from train import train_epoch
+from test import eval
+
+
+def main():
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+    # RESNET CODE --------
+    # model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
+    # encoder = torch.nn.Sequential(*(list(model.children())[:-1]))
+    # print(encoder)
+    # print(sum(p.numel() for p in encoder.parameters() if p.requires_grad))
+    # --------------------
+
+    BATCH_SIZE = 1
+    EPOCHS = 10
+    LR = 3e-4
+
+    dataset = torch.load("./data/ModelStealing.pt")
+    dataset.transform = Compose([
+        PILToTensor(),
+    ])
+    train_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True)
+
+    model = Encoder()
+    optimizer = optim.Adam(model.parameters(), lr=LR)
+    criterion = ContrastiveLoss(BATCH_SIZE)
+
+    # TRAINING
+    for epoch in range(EPOCHS):
+        train_epoch(device, model, criterion, optimizer, train_loader)
+        # cont_loss, l2_loss = eval(device, epoch, model, criterion, val_loader)
+
+
+if __name__ == '__main__':
+    main()
