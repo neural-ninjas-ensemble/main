cf4592a3ec31c1fdde15a484c890d272e072abb5
Piotr Kitłowski <piotr.kitlowski@gmail.com>
2024-03-16 16:30:44 +0100

    nowe api

--- Task 1/api_simulation.py
+++ Task 1/api_simulation.py
@@ -1,66 +1,131 @@
+from typing import List
+from dotenv import load_dotenv
+
+import numpy as np
 import requests
-def model_stealing(path_to_png_file: str):
-    SERVER_URL = "[paste server url here]"
-    ENDPOINT = "/modelstealing"
-    URL = SERVER_URL + ENDPOINT
+import os
 
-    TEAM_TOKEN = "[paste your team token here]"
+load_dotenv()
 
-    with open(path_to_png_file, "rb") as img_file:
-        response = requests.get(
-            URL, files={"file": img_file}, headers={"token": TEAM_TOKEN}
+SERVER_URL = os.getenv("SERVER_URL")
+TEAM_TOKEN = os.getenv("TEAM_TOKEN")
+
+
+def sybil(ids: List[int], home_or_defense: str, binary_or_affine: str):
+    if home_or_defense not in ["home", "defense"] or binary_or_affine not in [
+        "binary",
+        "affine",
+    ]:
+        raise Exception("Invalid endpoint")
+
+    endpoint = f"/sybil/{binary_or_affine}/{home_or_defense}"
+    url = SERVER_URL + endpoint
+    ids = ",".join(map(str, ids))
+    response = requests.get(url, params={"ids": ids}, headers={"token": TEAM_TOKEN})
+    if response.status_code == 200:
+        representations = response.json()["representations"]
+        ids = response.json()["ids"]
+        return representations
+    else:
+        raise Exception(
+            f"Sybil failed. Code: {response.status_code}, content: {response.json()}"
         )
 
-        if response.status_code == 200:
-            return response.content["representation"]
-        else:
-            raise Exception(f"Request failed. Status code: {response.status_code}, content: {response.content}")
-        
-def model_stealing_reset():
-    SERVER_URL = "[paste server url here]"
-    ENDPOINT = "/modelstealing/reset"
-    URL = SERVER_URL + ENDPOINT
 
-    TEAM_TOKEN = "[paste your team token here]"
+# Be careful. This can be done only 4 times an hour.
+# Make sure your file has proper content.
+def sybil_submit(binary_or_affine: str, path_to_npz_file: str):
+    if binary_or_affine not in ["binary", "affine"]:
+        raise Exception("Invalid endpoint")
 
-    response = requests.post(
-        URL, headers={"token": TEAM_TOKEN}
-    )
+    endpoint = f"/sybil/{binary_or_affine}/submit"
+    url = SERVER_URL + endpoint
+
+    with open(path_to_npz_file, "rb") as f:
+        response = requests.post(url, files={"file": f}, headers={"token": TEAM_TOKEN})
 
     if response.status_code == 200:
-        print("Endpoint rested successfully")
+        print("OK")
+        print(response.json())
     else:
-        raise Exception(f"Request failed. Status code: {response.status_code}, content: {response.content}")
-    
-    
-    
-    #ładownaie danych 
-    
-    
-    import torch
-from taskdataset import TaskDataset
-
-
-if __name__ == "__main__":
-    dataset = torch.load("modelstealing/data/ExampleModelStealingPub.pt")
-
-    print(dataset.ids, dataset.imgs, dataset.labels)
-    
-    
-    
-def model_stealing_submission(path_to_onnx_file: str):
-    SERVER_URL = "[paste server url here]"
-    ENDPOINT = "/modelstealing/submit"
-    URL = SERVER_URL + ENDPOINT
-
-    TEAM_TOKEN = "[paste your team token here]"
-
-    with open(path_to_onnx_file, "rb") as onnx_file:
-        response = requests.post(
-            URL, files={"file": onnx_file}, headers={"token": TEAM_TOKEN}
+        print(
+            f"Request submit failed. Status code: {response.status_code}, content: {response.json()}"
         )
 
+
+def sybil_reset(binary_or_affine: str, home_or_defense: str):
+    if binary_or_affine not in ["binary", "affine"]:
+        raise Exception("Invalid endpoint")
+    
+    if home_or_defense not in ["home", "defense"]:
+        raise Exception("Invalid endpoint")
+
+    endpoint = f"/sybil/{binary_or_affine}/reset/{home_or_defense}"
+    url = SERVER_URL + endpoint
+    response = requests.post(url, headers={"token": TEAM_TOKEN})
+    if response.status_code == 200:
+        print("Request ok")
+        print(response.json())
+    else:
+        raise Exception(
+            f"Sybil reset failed. Code: {response.status_code}, content: {response.json()}"
+        )
+
+
+# Be careful. This can be done only once an hour.
+# Computing this might take a few minutes. Be patient.
+# Make sure your file has proper content.
+def defense_submit(path_to_npz_file: str):
+    endpoint = "/defense/submit"
+    url = SERVER_URL + endpoint
+    with open(path_to_npz_file, "rb") as f:
+        response = requests.post(url, files={"file": f}, headers={"token": TEAM_TOKEN})
         if response.status_code == 200:
-            return response.content["score"]
+            print("Request ok")
+            print(response.json())
         else:
-            raise Exception(f"Request failed. Status code: {response.status_code}, content: {response.content}")
+            raise Exception(
+                f"Defense submit failed. Code: {response.status_code}, content: {response.json()}"
+            )
+
+
+def model_stealing(path_to_png_file: str):
+    endpoint = "/modelstealing"
+    url = SERVER_URL + endpoint
+    with open(path_to_png_file, "rb") as f:
+        response = requests.get(url, files={"file": f}, headers={"token": TEAM_TOKEN})
+        if response.status_code == 200:
+            representation = response.json()["representation"]
+            print("Request ok")
+            print(representation)
+        else:
+            raise Exception(
+                f"Model stealing failed. Code: {response.status_code}, content: {response.json()}"
+            )
+
+
+def model_stealing_submit(path_to_onnx_file: str):
+    endpoint = "/modelstealing/submit"
+    url = SERVER_URL + endpoint
+    with open(path_to_onnx_file, "rb") as f:
+        response = requests.post(url, files={"file": f}, headers={"token": TEAM_TOKEN})
+        if response.status_code == 200:
+            print("Request ok")
+            print(response.json())
+        else:
+            raise Exception(
+                f"Model stealing submit failed. Code: {response.status_code}, content: {response.json()}"
+            )
+
+
+def model_stealing_reset():
+    endpoint = f"/modelstealing/reset"
+    url = SERVER_URL + endpoint
+    response = requests.post(url, headers={"token": TEAM_TOKEN})
+    if response.status_code == 200:
+        print("Request ok")
+        print(response.json())
+    else:
+        raise Exception(
+            f"Model stealing reset failed. Code: {response.status_code}, content: {response.json()}"
+        )
