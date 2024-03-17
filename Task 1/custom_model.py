e251e65bf38943624ec169a6a8119336b1004c54
Wojciech Łapacz <wojciech.lapacz02@gmail.com>
2024-03-17 08:42:01 +0100

    device changed

--- Task 1/custom_model.py
+++ Task 1/custom_model.py
@@ -12,7 +12,13 @@ class Encoder(nn.Module):
             nn.GELU(),
             nn.Dropout2d(0.4),
 
-            nn.Conv2d(8, 16, kernel_size=5, stride=1, padding='same', bias=False),
+            nn.Conv2d(8, 16, kernel_size=3, stride=1, padding='same', bias=False),
+            nn.BatchNorm2d(16),
+            nn.GELU(),
+            nn.MaxPool2d(2),
+            nn.Dropout2d(0.25),
+
+            nn.Conv2d(16, 16, kernel_size=3, stride=1, padding='same', bias=False),
             nn.BatchNorm2d(16),
             nn.GELU(),
             nn.MaxPool2d(2),
