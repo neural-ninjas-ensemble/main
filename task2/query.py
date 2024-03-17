56ae5196b0e209e24f9a41451a57e8c10bcd8567
mlojek <mlojek@protonmail.com>
2024-03-17 11:11:05 +0000

    task2 final

--- task2/query.py
+++ task2/query.py
@@ -41,20 +41,20 @@ def make_user_queries(path_to_data: str, query_limit: int=2000, calib_len: int=1
     return ids_users
 
 
-def query_sybil_full_dataset_affine(data_path: str) -> pd.DataFrame:
-    sybil_reset('affine', 'home')
-    sybil_reset('affine', 'defense')
+def query_sybil_full_dataset_binary(data_path: str) -> pd.DataFrame:
+    sybil_reset('binary', 'home')
+    sybil_reset('binary', 'defense')
 
     # list of 19 lists each of 2000 ids
-    ids_users = make_user_queries(data_path)
+    ids_users = make_user_queries(data_path, calib_len=200)
 
     # result dict
     result = {}
 
     # user 0 home
     user0_calib, user0_new = ids_users[0]
-    user0_calib_reps = sybil(user0_calib, 'home', 'affine')
-    user0_new_reps = sybil(user0_new, 'home', 'affine')
+    user0_calib_reps = sybil(user0_calib, 'home', 'binary')
+    user0_new_reps = sybil(user0_new, 'home', 'binary')
     result[0] = {
         'calib': {
             img_id: rep for img_id, rep in zip(user0_calib, user0_calib_reps)
@@ -68,12 +68,63 @@ def query_sybil_full_dataset_affine(data_path: str) -> pd.DataFrame:
     i = 1
     user_num = len(ids_users)
     while i < user_num:
-        sybil_reset('affine', 'defense')
+        sybil_reset('binary', 'defense')
         print(i)
         try:
             this_calib, this_new = ids_users[i]
-            this_calib_reps = sybil(this_calib, 'defense', 'affine')
-            this_new_reps = sybil(this_new, 'defense', 'affine')
+            this_calib_reps = sybil(this_calib, 'defense', 'binary')
+            this_new_reps = sybil(this_new, 'defense', 'binary')
+            
+            result[i] = {
+                'calib': {
+                    img_id: rep for img_id, rep in zip(this_calib, this_calib_reps)
+                },
+                'new': {
+                    img_id: rep for img_id, rep in zip(this_new, this_new_reps)
+                }
+            }
+
+            i = i + 1
+        except Exception:
+            time.sleep(1)
+            print('sleep')
+
+    return result
+
+
+def query_sybil_full_dataset_binary(data_path: str) -> pd.DataFrame:
+    sybil_reset('binary', 'home')
+    sybil_reset('binary', 'defense')
+
+    # list of 19 lists each of 2000 ids
+    ids_users = make_user_queries(data_path, calib_len=200)
+
+    # result dict
+    result = {}
+
+    # user 0 home
+    user0_calib, user0_new = ids_users[0]
+    user0_calib_reps = sybil(user0_calib, 'home', 'binary')
+    user0_new_reps = sybil(user0_new, 'home', 'binary')
+    result[0] = {
+        'calib': {
+            img_id: rep for img_id, rep in zip(user0_calib, user0_calib_reps)
+        },
+        'new': {
+            img_id: rep for img_id, rep in zip(user0_new, user0_new_reps)
+        }
+    }
+    
+    # quer defense with users 1-18
+    i = 1
+    user_num = len(ids_users)
+    while i < user_num:
+        sybil_reset('binary', 'defense')
+        print(i)
+        try:
+            this_calib, this_new = ids_users[i]
+            this_calib_reps = sybil(this_calib, 'defense', 'binary')
+            this_new_reps = sybil(this_new, 'defense', 'binary')
             
             result[i] = {
                 'calib': {
@@ -93,6 +144,7 @@ def query_sybil_full_dataset_affine(data_path: str) -> pd.DataFrame:
 
 
 if __name__ == '__main__':
-    result = query_sybil_full_dataset_affine('/home/hack33/task2/SybilAttack.pt')
-    with open('sybil_data.json', 'w') as f:
+    # result = query_sybil_full_dataset_binary('/home/hack33/task2/SybilAttack.pt')
+    result = query_sybil_full_dataset_binary('/home/hack33/task2/SybilAttack.pt')
+    with open('sybil_data_binary.json', 'w') as f:
         json.dump(result, f, indent=4)
--- task2/revert_transforms.py
+++ task2/revert_transforms.py
@@ -5,15 +5,32 @@ import torch.optim as optim
 import numpy as np
 import json
 import scipy
-from sklearn.linear_model import Ridge
+from sklearn.linear_model import Ridge, LinearRegression
 import numpy as np
+from scipy.optimize import curve_fit
 
 from taskdataset import TaskDataset
+
+
+class LinReg(nn.Module):
+    def __init__(self, input_dim):
+        super(LinReg, self).__init__()
+        self.weight = nn.Parameter(torch.randn(1, input_dim))  # Weight vector
+        self.bias = nn.Parameter(torch.randn(input_dim))  # Bias vector
+
+    def forward(self, x):
+        return torch.mul(x, self.weight) + self.bias
 
 
 def revert_user_ridge(calib_dirty, calib_clean, x_dirty):
     clf = Ridge(alpha=1.0)
     clf.fit(calib_dirty, calib_clean)
+    return clf.predict(x_dirty).tolist()
+
+def revert_user_linearregress(calib_dirty, calib_clean, x_dirty):
+    clf = LinearRegression()
+    clf.fit(calib_dirty, calib_clean)
+    print(clf.score(calib_dirty, calib_clean))
     return clf.predict(x_dirty).tolist()
 
 
@@ -23,6 +40,17 @@ def revert_user_ridge(calib_dirty, calib_clean, x_dirty):
 #     A = np.outer(calib_dirty, x_inv)
 #     return scipy.linalg.inv(A)
 
+class KDLoss(nn.Module):
+    def __init__(self, T=1):
+        super(KDLoss, self).__init__()
+        self.T = T
+
+    def forward(self, our_emb, target_emb):
+        soft_targets = nn.functional.softmax(target_emb / self.T, dim=-1)
+        soft_prob = nn.functional.log_softmax(our_emb / self.T, dim=-1)
+
+        soft_targets_loss = torch.sum(soft_targets * (soft_targets.log() - soft_prob)) / soft_prob.size()[0] * (self.T ** 2)
+        return soft_targets_loss
 
 def revert_user(calib_dirty, calib_clean, x_dirty):
     '''
@@ -30,7 +58,13 @@ def revert_user(calib_dirty, calib_clean, x_dirty):
     returns x_clean
     '''
     # Create an instance of the model
-    model = nn.Linear(384, 384)
+    model = nn.Sequential(
+        nn.Linear(384, 384),
+        nn.ReLU(),
+        nn.Linear(384, 384),
+        # nn.Sigmoid()
+    )
+    # model = LinReg(384)
 
     # Convert data to PyTorch tensors
     inputs = torch.tensor(calib_dirty)
@@ -39,7 +73,7 @@ def revert_user(calib_dirty, calib_clean, x_dirty):
 
     # Define loss function and optimizer
     criterion = nn.MSELoss()
-    optimizer = optim.SGD(model.parameters(), lr=0.4)
+    optimizer = optim.SGD(model.parameters(), lr=0.5, momentum=0.9)
 
     # Training loop
     num_epochs = 5000
@@ -47,6 +81,12 @@ def revert_user(calib_dirty, calib_clean, x_dirty):
         # TODO add validation part
         # Forward pass
         outputs = model(inputs)
+        # print(outputs.type)
+        # outputs = torch.where(outputs > 0.9, torch.ones_like(outputs), outputs)
+        # outputs = torch.where(outputs < 0.1, torch.zeros_like(outputs), outputs)
+        # thresh1 = torch.tensor([0.75])
+        # outputs = (outputs > thresh1)
+        # outputs = torch.max(torch.zeros(output.shape), torch.minimum(output, torch.ones(output.shape)))
         loss = criterion(outputs, targets)
 
         # Backward pass and optimization
@@ -55,10 +95,12 @@ def revert_user(calib_dirty, calib_clean, x_dirty):
         optimizer.step()
 
         # if (epoch+1) % 100 == 0:
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.6f}')
 
     # Save the trained model
     x_clean_tensor = model(to_translate)
+    # x_clean_tensor = torch.where(x_clean_tensor > 0.9, torch.ones_like(x_clean_tensor), x_clean_tensor)
+    # x_clean_tensor = torch.where(x_clean_tensor < 0.1, torch.zeros_like(x_clean_tensor), x_clean_tensor)
 
     return x_clean_tensor.tolist()
 
@@ -103,10 +145,12 @@ def revert_transforms(data: dict) -> dict:
     return(data_clean)
 
 
+def cap01(values):
+    return [max(0, min(1, val)) for val in values]
 
 
 if __name__ == '__main__':
-    with open('sybil_data.json', 'r') as f:
+    with open('sybil_data_affine.json', 'r') as f:
         data = json.load(f)
 
     reverted_data = revert_transforms(data)
