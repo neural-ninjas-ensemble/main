ee6b0046d3ba01ef6486e2b84448b1e28513ed9e
Piotr Kitłowski <piotr.kitlowski@gmail.com>
2024-03-17 08:37:52 +0100

    task3

---
+++ Task 3/utils.py
@@ -0,0 +1,40 @@
+import torch
+import pandas as pd
+
+
+def save_model(model, hour):
+    filename = f"./models/model_{hour}"
+    # SAVE TO PTH
+    torch.save(model.state_dict(), filename + ".pth")
+
+    # SAVE TO ONNX
+    torch.onnx.export(
+        model.to(torch.device("cpu")),
+        torch.randn(1, 3, 32, 32),
+        filename + ".onnx",
+        export_params=True,
+        input_names=["x"],
+    )
+
+
+def save_history(history, loss_name, hour):
+    filename = f"./reports/report_{hour}"
+    df = pd.DataFrame(history)
+    df.columns = [loss_name, "l2_loss"]
+    df.to_csv(filename + ".csv", index=False)
+
+
+def get_position_by_id(ids, dataset):
+    df = pd.DataFrame([dataset.ids]).T
+    df.columns = ['id']
+    return torch.from_numpy(df.index[df['id'].isin(ids)].values)
+
+
+def get_class_dict():
+    dataset = torch.load("./data/ModelStealing.pt")
+    df = pd.DataFrame([dataset.ids,dataset.labels]).T
+    df.columns =['id', 'labels']
+    unique = df.labels.unique()
+    nunique_ids = df.labels.nunique()
+    dict_transform = {unique[i]: i for i in range(nunique_ids)}
+    return dict_transform
